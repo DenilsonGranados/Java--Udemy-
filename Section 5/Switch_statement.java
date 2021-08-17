87b4c6fc03bc84fcc11dbe71c5ed76da572d7e79
Denilson Granados <dbg307@nyu.edu>
2021-08-16 20:51:29 -0400

    1.01 commit

--- Section 5/Switch_statement.java
+++ Section 5/Switch_statement.java
@@ -1,3 +1,18 @@
 public class Switch_statement {
-    
+    public static void main(String[] args){
+        int swaitchValue=1;
+        switch(swaitchValue){
+            case 1:
+            System.out.println("Value was 1");
+            break;
+
+            case 2:
+            System.out.println("Value was 2");
+            break;
+
+            default:
+            System.out.println("Was not 1 or 2");
+            break;
+        }
+    }
 }
